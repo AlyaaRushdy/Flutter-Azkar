a901b3fc29d0cacbd565c7467ad9e2aa488eb704
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2024-03-25 08:29:15 +0200

    home page

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:session11_task/widgets/zikr.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Azkar",
+        ),
+      ),
+      body: const Column(
+        children: [
+          Zikr(text: "سبحان الله", counter: 0),
+          Zikr(text: "الحمد الله", counter: 0),
+          Zikr(text: "لا اله الا الله", counter: 0),
+          Zikr(text: " الله اكبر", counter: 0),
+        ],
+      ),
+    );
+  }
+}
