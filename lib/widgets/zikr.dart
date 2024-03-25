db82bfda25f0992bb24f67e0c5030f2cf94d4581
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2024-03-25 08:29:30 +0200

    zikr row

---
+++ lib/widgets/zikr.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:session11_task/widgets/counter_button.dart';
+
+class Zikr extends StatelessWidget {
+  const Zikr({
+    super.key,
+    required this.text,
+    required this.counter,
+  });
+
+  final String text;
+  final int counter;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        textDirection: TextDirection.rtl,
+        children: [
+          Container(
+            constraints: const BoxConstraints(maxWidth: 300),
+            child: Text(
+              text,
+              overflow: TextOverflow.visible,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 26,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          CounterButton(counter: counter),
+        ],
+      ),
+    );
+  }
+}
